3deae349c5d7ef16167c79940a29726fb63654a7
yuKalasnikova2 <pmalian@list.ru>
2023-05-10 14:00:14 +0700

    feat(styles): add styles for Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [text, setText] = useState('')
+
+  const handleAddTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: new Date().toISOString(),
+        text,
+        completed: false,
+      },
+    ])
+    setText('')
+  }
   return (
-    <>
+    <div className="main">
       <h1>Туду лист + редакс</h1>
-    </>
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={handleAddTodo}> Add todo</button>
+      </label>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" />
+            <span>{todo.text}</span>
+            <span className='delete'>&times;</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
